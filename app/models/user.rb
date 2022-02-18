f18de18fab7c04937efcd70027f11fbb0634df87
JakeGiangan <jake@kodakollectiv.com>
2022-02-18 10:20:31 +0800

    added seed and updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :name, length: { minimum: 2, maximum: 100}, presence: true
     validates :email, length: { minimum: 2, maximum: 100}, presence: true
+    validates :password, length: { minimum: 4 }, presence: true
 end
